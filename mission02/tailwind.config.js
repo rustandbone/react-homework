cbb9188b214aa1048d97e8d37e7e92763f38fd5b
rustandbone <dlehdgh14@naver.com>
2023-08-20 01:06:14 +0900

    tailwind 폰트, 색상 설정

--- mission02/tailwind.config.js
+++ mission02/tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.jsx"],
+  content: ['./index.html', './src/**/*.jsx'],
   theme: {
     extend: {
       fontFamily: {
-        suit: ["SUIT Variable"],
+        sans: ['Pretendard', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        white: '#fff',
+        black: '#000',
+        gray1: '#E1E1E1',
+        gray2: '#C4C4C4',
+        gray3: '#A6A6A6',
+        gray4: '#898989',
+        gray5: '#6B6B6B',
+        gray6: '#565656',
+        gray7: '#404040',
+        gray8: '#2B2B2B',
+        gray9: '#151515',
       },
     },
   },
