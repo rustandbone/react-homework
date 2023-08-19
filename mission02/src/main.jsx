2517889dc66a10771aedbf50aecd2d263d0d4afa
rustandbone <dlehdgh14@naver.com>
2023-08-20 01:07:36 +0900

    Router 추가

--- mission02/src/main.jsx
+++ mission02/src/main.jsx
@@ -1,11 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import './index.css'
+import router from './routes';
 import './styles/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
