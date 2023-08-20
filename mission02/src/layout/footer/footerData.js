f29e43151829ff4216414b3cda4fc9d541ac6c93
rustandbone <dlehdgh14@naver.com>
2023-08-20 19:11:16 +0900

    footer data

---
+++ mission02/src/layout/footer/footerData.js
@@ -0,0 +1,16 @@
+export const footerMenuList = [
+  `고객센터`,
+  `이용약관`,
+  `개인정보처리방침`,
+  `청소년보호정책`,
+  '법적고지',
+  '이벤트',
+  '인재채용',
+];
+
+export const footerSnsList = [
+  { 유튜브: 'youtube' },
+  { 인스타그램: 'instagram' },
+  { 트위터: 'twitter' },
+  { 페이스북: 'facebook' },
+];
