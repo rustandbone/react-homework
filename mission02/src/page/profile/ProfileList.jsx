3c6354388b8a004ea1d8e0bf141406eeb0f2369d
rustandbone <dlehdgh14@naver.com>
2023-08-20 19:12:06 +0900

    pocketbase로 변경

--- mission02/src/page/profile/ProfileList.jsx
+++ mission02/src/page/profile/ProfileList.jsx
@@ -5,12 +5,12 @@ export default function ProfileList({selected, icon}) {
 
   useEffect(() => {
     async function fetchlists() {
-      const response = await fetch('/server/db/data.json');
+      const response = await fetch("http://127.0.0.1:3000/api/collections/profile/records")
       if(!response.ok) {
         console.log(response.ok)
       }
       const data = await response.json();
-      setList(Object.entries(data.users))
+      setList(data.items)
     }
     fetchlists();
   }, [])
@@ -20,7 +20,7 @@ export default function ProfileList({selected, icon}) {
       {
         list?.map((item) => { 
           return (
-            <li key={item[0]} className="text-gray3">
+            <li key={item.nickname} className="text-gray3">
               <a
                 href={`./${icon === 'lock' ? '' : 'edit'}`}
                 className="text-gray3"
@@ -29,17 +29,17 @@ export default function ProfileList({selected, icon}) {
                   className="relative duration-500 ease-linear hover:-translate-y-[5%]"
                 >
                   <img
-                    src={`./src/assets/${item[1]}`}
-                    alt={`${item[0]} 프로필`}
+                    src={`./src/assets/${item.src}`}
+                    alt={`${item.nickname} 프로필`}
                     className="w-40 flex-col gap-y-4 rounded"
                   />
                   <div
                     className={`absolute bottom-0 left-0 right-0 top-0 rounded 
-                      ${item[0] !== selected ? 'bg-black/[.5]' : ''}`}
+                      ${item.nickname === selected && icon === 'lock' ? '' : 'bg-black/[.5]'}`}
                   >
                     {
                       (icon === 'lock' &&
-                      item[0] === selected) || 
+                      item.nickname === selected) || 
                       <img
                         src={`./src/assets/${icon}.png`}
                         alt="자물쇠"
@@ -49,7 +49,7 @@ export default function ProfileList({selected, icon}) {
                     }
                   </div>
                 </div>
-                <p className="py-4 text-center text-xl text-gray3">{item[0]}</p>
+                <p className="py-4 text-center text-xl text-gray3">{item.nickname}</p>
               </a>
             </li>
           )
