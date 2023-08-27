b8d2fbb4ce559c9c798446dd48bdf641e4160e99
rustandbone <dlehdgh14@naver.com>
2023-08-27 22:40:56 +0900

    인증, helmet 적용

--- mission03/src/main.jsx
+++ mission03/src/main.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import router from './routes';
 import './styles/tailwind.css'
+import AuthProvider from './contexts/Auth';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={router} />  
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
