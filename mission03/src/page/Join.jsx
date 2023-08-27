129c4e087e33e7414d9333d30c823258824e956b
rustandbone <dlehdgh14@naver.com>
2023-08-27 22:42:17 +0900

    인증 확인 추가

--- mission03/src/page/Join.jsx
+++ mission03/src/page/Join.jsx
@@ -1,8 +1,11 @@
 import pb from "@/api/pocketbase";
+import { useAuth } from "@/contexts/Auth";
 import { useState } from "react"
-import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export default function Join() {
+  const {isAuth} = useAuth();
   const navigate = useNavigate();
 
   const [formState, setFormState] = useState({
@@ -12,9 +15,13 @@ export default function Join() {
     passwordConfirm:''
   })
 
+  if(isAuth) {
+    return <Navigate to="/profile" />;
+  }
+
   const handleRegister = async (e) => {
     e.preventDefault();
-    //PocketBase SDK 인증 요청
+    
     const {password, passwordConfirm} = formState;
     if(password !== passwordConfirm) {
       alert('비밀번호가 일치하지 않습니다. 다시 확인해보세요.')
@@ -37,173 +44,180 @@ export default function Join() {
       ...formState,
       [name]:value
     })
-    console.log(formState)
   }
 
   return (
-    <section className="join mx-[auto] my-14 w-2/5 max-w-[400px]">
-      <article className="title_wrap mb-[25px] text-center text-xl font-bold">
-        <h3>
-          티빙 회원가입
-          <p className="mt-2.5 text-sm font-normal text-gray3">
-            아이디와 이메일로 간편하게 티빙을 시작하세요!
-          </p>
-        </h3>
-      </article>
+    <>
+      <Helmet>
+        <title>Taing - 회원가입</title>
+      </Helmet>
+      <section className="join mx-[auto] my-14 w-2/5 max-w-[400px]">
+        <article className="title_wrap mb-[25px] text-center text-xl font-bold">
+          <h3>
+            티빙 회원가입
+            <p className="mt-2.5 text-sm font-normal text-gray3">
+              아이디와 이메일로 간편하게 티빙을 시작하세요!
+            </p>
+          </h3>
+        </article>
 
-      <form onSubmit={handleRegister}>
-        <div className="make_id">
-          <label htmlFor="userId" className="data_input_id layout_flex sr-only">아이디</label>
-          <input
-            type="text"
-            name="username"
-            id="userId"
-            className="new_id mt-2.5 w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-none focus:border-[1px] focus:border-gray3"
-            required
-            placeholder="아이디"
-            defaultValue={formState.name}
-            onChange={handleInput}
-          />
-          <p className="input_info mt-[5px] text-[10px] font-medium text-gray5">
-            영문 또는 영문, 숫자 조합 6~12자리
-          </p>
-        </div>
-
-        <div className="make_pw relative">
-          <label htmlFor="userPw" className="data_input_pw layout_flex sr-only">비밀번호</label>
-          <input
-            type="password"
-            id="userPw"
-            name="password"
-            className="new_pw mt-2.5 w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-none focus:border-[1px] focus:border-gray3"
-            required
-            placeholder="비밀번호"
-            defaultValue={formState.password}
-            onChange={handleInput}
-          />
-          <button
-            id="closePw"
-            className="brightness-3 absolute bottom-[39px] right-6 h-[25px] w-[25px] bg-[url('/icon/DesktopPw.png')] bg-contain bg-[25px] invert-[50%]"
-          ></button>
-          <p className="input_pw mt-[5px] text-[10px] font-medium text-gray5">
-            영문,숫자,특수문자(~!@#$%^&*) 조합 8~15자리
-          </p>
-        </div>
-
-        <div className="make_pw make_check_pw relative">
-          <label
-            htmlFor="passwordConfirm"
-            className="data_input_check layout_flex sr-only"
-            >비밀번호 확인
-          </label>
-          <input
-            type="password"
-            id="passwordConfirm"
-            name="passwordConfirm" 
-            className="new_pw_check mt-2.5 w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-none focus:border-[1px] focus:border-gray3"
-            required
-            placeholder="비밀번호 확인"
-            defaultValue={formState.passwordConfirm}
-            onChange={handleInput}
-          />
-          <button
-            id="closePwCheck"
-            className="brightness-3 absolute right-6 top-[29px] h-[25px] w-[25px] bg-[url('/icon/DesktopPw.png')] bg-contain bg-[25px] invert-[50%]"
-          ></button>
-
-          <p className="input_checkpw mt-[3px] hidden text-[10px] font-medium text-gray5">
-            비밀번호가 일치합니다.
-          </p>
-          <p className="input_checkpw_N mt-[3px] hidden text-[10px] font-medium text-red">
-            비밀번호가 일치하지 않습니다.
-          </p>
-        </div>
-        
-        <div className="make_email">
-          <label htmlFor="email" className="data_input_email layout_flex sr-only"
-            >이메일</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            className="new_email mt-2.5 w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-none focus:border-[1px] focus:border-gray3"
-            required
-            placeholder="이메일"
-            defaultValue={formState.email}
-            onChange={handleInput}
-          />
-        </div>
-        <p
-          className="input_email mt-[4px] hidden text-[10px] font-medium text-gray5"
-        >이메일 형식이 일치합니다.</p>
-        <p
-          className="input_email_N mt-[4px] hidden text-[10px] font-medium text-red"
-        >이메일 형식이 일치하지 않습니다.</p>
-
-        <ul className="check_wrap mb-[13px] mt-3 text-[13px] font-light text-gray2">
-          <li className="check_list">
+        <form onSubmit={handleRegister}>
+          <div className="make_id">
+            <label htmlFor="userId" className="data_input_id layout_flex sr-only">아이디</label>
             <input
-              className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopAFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
-              type="checkbox"
-              id="agreeAll"
+              type="text"
+              name="username"
+              id="userId"
+              className="new_id mt-2.5 w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-none focus:border-[1px] focus:border-gray3"
+              required
+              placeholder="아이디"
+              minLength='6'
+              defaultValue={formState.name}
+              onChange={handleInput}
             />
-            <label htmlFor="agreeAll" className="cursor-pointer">
-              <span className="pl-[5px] align-middle">모두 동의합니다.</span>
-            </label>
-          </li>
-        </ul>
-        <ul className="checks_wrap">
-          <li className="check_box mb-[15px]">
-            <input
-              type="checkbox"
-              className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDNoFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopANoFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
-              id="agree_1"
-            />
-            <label htmlFor="agree_1" className="cursor-pointer text-gray5">
-              만 14세 이상입니다.
-            </label>
-          </li>
-          <li className="check_box mb-[15px]">
-            <input
-              type="checkbox"
-              className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDNoFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopANoFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
-              id="agree_2"
-            />
-            <label htmlFor="agree_2" className="cursor-pointer text-gray5">
-              [필수] 서비스 이용약관 동의
-            </label>
-          </li>
-          <li className="check_box mb-[15px]">
-            <input
-              type="checkbox"
-              className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDNoFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopANoFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
-              id="agree_3"
-            />
-            <label htmlFor="agree_3" className="cursor-pointer text-gray5">
-              [필수] 개인정보 수집 및 서비스 활용 동의
-            </label>
-          </li>
-          <li className="check_box mb-[15px]">
-            <input
-              type="checkbox"
-              className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDNoFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopANoFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
-              id="agree_4"
-            />
-            <label htmlFor="agree_4" className="cursor-pointer text-gray5">
-              [필수] 채널 홈페이지 개인정보 제 3자 제공 동의
-            </label>
-          </li>
-        </ul>
+            <p className="input_info mt-[5px] text-[10px] font-medium text-gray5">
+              영문 또는 영문, 숫자 조합 6~12자리
+            </p>
+          </div>
 
-      <button
-        type="submit"
-        id="btnRegist"
-        className="regist_btn mx-0 mb-10 mt-2.5 inline-block w-full rounded bg-gray7 px-0 py-[15px] text-gray3"
-      >
-        가입하기
-      </button>
-      </form>
+          <div className="make_pw relative">
+            <label htmlFor="userPw" className="data_input_pw layout_flex sr-only">비밀번호</label>
+            <input
+              type="password"
+              id="userPw"
+              name="password"
+              className="new_pw mt-2.5 w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-none focus:border-[1px] focus:border-gray3"
+              required
+              minLength='8'
+              placeholder="비밀번호"
+              defaultValue={formState.password}
+              onChange={handleInput}
+            />
+            <button
+              id="closePw"
+              className="brightness-3 absolute bottom-[39px] right-6 h-[25px] w-[25px] bg-[url('/icon/DesktopPw.png')] bg-contain bg-[25px] invert-[50%]"
+            ></button>
+            <p className="input_pw mt-[5px] text-[10px] font-medium text-gray5">
+              영문,숫자,특수문자(~!@#$%^&*) 조합 8~15자리
+            </p>
+          </div>
 
-    </section>
+          <div className="make_pw make_check_pw relative">
+            <label
+              htmlFor="passwordConfirm"
+              className="data_input_check layout_flex sr-only"
+              >비밀번호 확인
+            </label>
+            <input
+              type="password"
+              id="passwordConfirm"
+              name="passwordConfirm" 
+              className="new_pw_check mt-2.5 w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-none focus:border-[1px] focus:border-gray3"
+              required
+              minLength='8'
+              placeholder="비밀번호 확인"
+              defaultValue={formState.passwordConfirm}
+              onChange={handleInput}
+            />
+            <button
+              id="closePwCheck"
+              className="brightness-3 absolute right-6 top-[29px] h-[25px] w-[25px] bg-[url('/icon/DesktopPw.png')] bg-contain bg-[25px] invert-[50%]"
+            ></button>
+
+            <p className="input_checkpw mt-[3px] hidden text-[10px] font-medium text-gray5">
+              비밀번호가 일치합니다.
+            </p>
+            <p className="input_checkpw_N mt-[3px] hidden text-[10px] font-medium text-red">
+              비밀번호가 일치하지 않습니다.
+            </p>
+          </div>
+          
+          <div className="make_email">
+            <label htmlFor="email" className="data_input_email layout_flex sr-only"
+              >이메일</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="new_email mt-2.5 w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-none focus:border-[1px] focus:border-gray3"
+              required
+              placeholder="이메일"
+              defaultValue={formState.email}
+              onChange={handleInput}
+            />
+          </div>
+          <p
+            className="input_email mt-[4px] hidden text-[10px] font-medium text-gray5"
+          >이메일 형식이 일치합니다.</p>
+          <p
+            className="input_email_N mt-[4px] hidden text-[10px] font-medium text-red"
+          >이메일 형식이 일치하지 않습니다.</p>
+
+          <ul className="check_wrap mb-[13px] mt-3 text-[13px] font-light text-gray2">
+            <li className="check_list">
+              <input
+                className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopAFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
+                type="checkbox"
+                id="agreeAll"
+              />
+              <label htmlFor="agreeAll" className="cursor-pointer">
+                <span className="pl-[5px] align-middle">모두 동의합니다.</span>
+              </label>
+            </li>
+          </ul>
+          <ul className="checks_wrap">
+            <li className="check_box mb-[15px]">
+              <input
+                type="checkbox"
+                className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDNoFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopANoFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
+                id="agree_1"
+              />
+              <label htmlFor="agree_1" className="cursor-pointer text-gray5">
+                만 14세 이상입니다.
+              </label>
+            </li>
+            <li className="check_box mb-[15px]">
+              <input
+                type="checkbox"
+                className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDNoFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopANoFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
+                id="agree_2"
+              />
+              <label htmlFor="agree_2" className="cursor-pointer text-gray5">
+                [필수] 서비스 이용약관 동의
+              </label>
+            </li>
+            <li className="check_box mb-[15px]">
+              <input
+                type="checkbox"
+                className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDNoFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopANoFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
+                id="agree_3"
+              />
+              <label htmlFor="agree_3" className="cursor-pointer text-gray5">
+                [필수] 개인정보 수집 및 서비스 활용 동의
+              </label>
+            </li>
+            <li className="check_box mb-[15px]">
+              <input
+                type="checkbox"
+                className="cursor-pointer appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDNoFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopANoFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
+                id="agree_4"
+              />
+              <label htmlFor="agree_4" className="cursor-pointer text-gray5">
+                [필수] 채널 홈페이지 개인정보 제 3자 제공 동의
+              </label>
+            </li>
+          </ul>
+
+        <button
+          type="submit"
+          id="btnRegist"
+          className="regist_btn mx-0 mb-10 mt-2.5 inline-block w-full rounded bg-gray7 px-0 py-[15px] text-gray3"
+        >
+          가입하기
+        </button>
+        </form>
+
+      </section>
+    </>
   )
 }
--- mission03/src/page/Login.jsx
+++ mission03/src/page/Login.jsx
@@ -1,9 +1,13 @@
 import pb from "@/api/pocketbase";
+import { useAuth } from "@/contexts/Auth";
 import { useState } from "react";
-import { useNavigate, useLocation, Link, NavLink } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { Navigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 
 export default function Login() {
-  const { state } = useLocation();
+  const {isAuth} = useAuth();
+  
   const navigate = useNavigate();
   
   const [formState, setFormState] = useState({
@@ -11,21 +15,21 @@ export default function Login() {
     password:'',
   })
 
+  if(isAuth) {
+    return <Navigate to="/profile" />;
+  }
+
   const handleSignIn = async (e) => {
     e.preventDefault();
-    //PocketBase SDK 인증 요청
+    
     const { username, password } = formState;
 
     try {
       await pb.collection('users').authWithPassword(username, password)
-
-      // if(!state) {
-        navigate('/profile');
-      // } else {
-      //   navigate(state.wishLocationPath)
-      // } 
+      navigate('/profile');
     } catch (error) {
       console.error(error);
+      alert('아이디나 비밀번호를 다시 확인해주세요.')
     }
   }
 
@@ -38,82 +42,89 @@ export default function Login() {
   }
 
   return (
-    <section
-      className="login mx-[auto] my-14 w-2/5 max-w-[400px]"
-    >
-      <div className="content_wrap">
-        <div className="title_wrap">
-          <h3 className="title mb-[25px] text-center text-xl font-bold">
-            TAING ID 로그인
-          </h3>
+    <>
+      <Helmet>
+        <title>Taing - 로그인</title>
+      </Helmet>
+      <section
+        className="login mx-[auto] my-14 w-2/5 max-w-[400px]"
+      >
+        <div className="content_wrap">
+          <div className="title_wrap">
+            <h3 className="title mb-[25px] text-center text-xl font-bold">
+              TAING ID 로그인
+            </h3>
+          </div>
         </div>
-      </div>
 
-      <form name="input_form" onSubmit={handleSignIn}>
-        <label htmlFor="id_input" className="data_input sr-only">아이디</label>
-        <input
-          type="text"
-          className="id_box mb-2.5 block w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-[0]"
-          id="id_input"
-          name="username" 
-          placeholder="아이디"
-          required="required"
-          defaultValue={formState.username} 
-          onChange={handleInput}
-        />
-        <span
-          className="error-message m-2 hidden text-xs text-red_login"
-          id="userEmailError">아이디는 6자리 이상 10자리 이하로 입력해 주세요.</span>
-        <div className="relative mb-2.5">
-          <label htmlFor="password_input" className="data_input sr-only"
-            >비밀번호
-          </label>
+        <form name="input_form" onSubmit={handleSignIn}>
+          <label htmlFor="id_input" className="data_input sr-only">아이디</label>
           <input
-            type="password"
-            className="pw_box w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-[0]"
-            id="password_input"
-            name="password" 
-            placeholder="비밀번호"
+            type="text"
+            className="id_box mb-2.5 block w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-[0]"
+            id="id_input"
+            name="username" 
+            placeholder="아이디"
             required="required"
-            autoComplete="current-password"
-            defaultValue={formState.password} 
+            minLength='6'
+            defaultValue={formState.username} 
             onChange={handleInput}
           />
-          <button
-            id="closePW"
-            className="brightness-3 absolute right-6 top-[20px] h-[25px] w-[25px] bg-[url('/icon/DesktopPw.png')] bg-contain bg-[25px] invert-[50%]"
-          ></button>
           <span
             className="error-message m-2 hidden text-xs text-red_login"
-            id="userPasswordError"
-            >비밀번호는 특수문자 포함 10자리 이상으로 입력해 주세요.</span>
-        </div>
-      <ul className="checks_wrap">
-        <li className="checks">
-          <input
-            className="appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopAFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
-            id="check"
-            type="checkbox"
-          />
-          <label htmlFor="check">
-            <span className="pl-[5px] align-middle">자동로그인</span>
-          </label>
-        </li>
-      </ul>
+            id="userEmailError">아이디는 6자리 이상 10자리 이하로 입력해 주세요.</span>
+          <div className="relative mb-2.5">
+            <label htmlFor="password_input" className="data_input sr-only"
+              >비밀번호
+            </label>
+            <input
+              type="password"
+              className="pw_box w-full rounded border-0 bg-darkbg2 px-[15px] py-5 text-white outline-[0]"
+              id="password_input"
+              name="password" 
+              placeholder="비밀번호"
+              required="required"
+              minLength='8'
+              autoComplete="current-password"
+              defaultValue={formState.password} 
+              onChange={handleInput}
+            />
+            <button
+              id="closePW"
+              className="brightness-3 absolute right-6 top-[20px] h-[25px] w-[25px] bg-[url('/icon/DesktopPw.png')] bg-contain bg-[25px] invert-[50%]"
+            ></button>
+            <span
+              className="error-message m-2 hidden text-xs text-red_login"
+              id="userPasswordError"
+              >비밀번호는 특수문자 포함 10자리 이상으로 입력해 주세요.</span>
+          </div>
+        <ul className="checks_wrap">
+          <li className="checks">
+            <input
+              className="appearance-none before:inline-block before:h-[17px] before:w-[17px] before:bg-[url('/icon/DesktopDFilled.png')] before:bg-[length:17px_17px] before:align-middle before:content-[''] checked:before:inline-block checked:before:h-[17px] checked:before:w-[17px] checked:before:bg-[url('/icon/DesktopAFilled.png')] checked:before:bg-[length:17px_17px] checked:before:bg-no-repeat"
+              id="check"
+              type="checkbox"
+            />
+            <label htmlFor="check">
+              <span className="pl-[5px] align-middle">자동로그인</span>
+            </label>
+          </li>
+        </ul>
 
-        <button
-          id="doLogIn"
-          className="btn_color bg- mx-0 mb-10 mt-5 inline-block w-full rounded bg-red_login px-0 py-[15px] text-white"
-        >
-          로그인
-        </button>
-      </form>
-      <div className="join_wrap flex justify-center gap-2.5 text-gray5">
-        &quot;아직 계정이 없으신가요?&quot;
-        <NavLink to="/join">
-          <span id="regist" className="no_border text-gray3">회원가입</span>
-        </NavLink>
-      </div>
-    </section>
+          <button
+            id="doLogIn"
+            className="btn_color bg- mx-0 mb-10 mt-5 inline-block w-full rounded bg-red_login px-0 py-[15px] text-white"
+          >
+            로그인
+          </button>
+        </form>
+        <div className="join_wrap flex justify-center gap-2.5 text-gray5">
+          &quot;아직 계정이 없으신가요?&quot;
+          <NavLink to="/join">
+            <span id="regist" className="no_border text-gray3">회원가입</span>
+          </NavLink>
+        </div>
+      </section>
+    </>
   )
 }
