1841c5c410c924b0541789d4ef2cf1f35c17eb45
rustandbone <dlehdgh14@naver.com>
2023-08-27 22:42:00 +0900

    로고 경로 수정

--- mission03/src/layout/Header.jsx
+++ mission03/src/layout/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header>
       <h1 className="mt-4 ml-10">
-        <a href="./">
+        <a href="/">
           <img src="/Logo_1.png" alt="TAING" />
         </a>
       </h1>
