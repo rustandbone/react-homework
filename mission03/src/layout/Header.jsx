104336ff27b77d2014fddb964cfe45c9ad3e29e0
rustandbone <dlehdgh14@naver.com>
2023-08-27 23:30:41 +0900

    로그아웃 기능 구현

--- mission03/src/layout/Header.jsx
+++ mission03/src/layout/Header.jsx
@@ -1,11 +1,26 @@
+import { useAuth } from "@/contexts/Auth";
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const { isAuth, signOut } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSignOut = async () => {
+    await signOut();
+    navigate('./')
+  }
+
   return (
-    <header>
+    <header className="flex justify-between">
       <h1 className="mt-4 ml-10">
         <a href="./">
           <img src="./Logo_1.png" alt="TAING" />
         </a>
       </h1>
+      {
+        isAuth &&
+        <button type="button" onClick={handleSignOut} className="mt-4 mr-10">로그아웃</button>
+      }
     </header>
   )
 }
